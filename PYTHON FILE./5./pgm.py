fbe7aa3f686eba89ceaa26969ed901e8a5342c69
navyapp <79969396+navyapp@users.noreply.github.com>
2022-02-08 21:33:13 +0530

    commit5

--- PYTHON FILE./5./pgm.py
+++ PYTHON FILE./5./pgm.py
@@ -1,1 +1,8 @@
 
+import filecmp
+f1=input("enter the first file name : ")
+f2=input("enter the second file name : ")
+result=filecmp.cmp(f1,f2)
+print(result)
+result=filecmp.cmp(f1,f2,shallow="false")
+print(result)
